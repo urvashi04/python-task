70ac96c46a60d4d109c56178a6a67184cf0eed2e
urvashi04 <51355239+urvashi04@users.noreply.github.com>
2019-06-09 22:01:47 +0000

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+x = [1,2,3,1,45,66,22,2,6,0,9]
+y=int(5)
+z=int(2)
+
+print("list of  elements greater than 5:")
+for i in x:
+  if y<i:
+     print(i)
+  
+print("list of element lessthan or equal to 2:")
+for i in x:
+  if z>=i:
+     print(i)
+     
+
